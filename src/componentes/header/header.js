f61134160abb0f70034ff84ad9e01f35b97b5ca5
ColiJD <colindresj9@gmail.com>
2023-04-12 01:45:33 -0600

    Primeros pasos con react

---
+++ src/componentes/header/header.js
@@ -0,0 +1,8 @@
+import "./header.css"
+function Header() {
+    return <header className="header">
+        <img className="header_img" src="img/Header.png" alt="Header" />
+    </header>
+
+}
+export default Header
